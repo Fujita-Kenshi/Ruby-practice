a8a79112bb86c8dd2706a6ba26ddda29eea80177
EC2 Default User <ec2-user@ip-172-31-78-254.ec2.internal>
2022-05-11 14:30:24 +0000

    rb_cp2

---
+++ r_cp2/age.rb
@@ -0,0 +1,6 @@
+# to_s メソッドを「.」接続(.to_s)することで、整数が、integerがstringに変換される。
+
+puts "Fujitaの年齢は" + 26.to_s + "です"
+# Fujitaの年齢は    : 文字列 string
+# 26                : 整数 integer
+# です              : 文字列 : string"
