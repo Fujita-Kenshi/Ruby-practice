cd728c6c801043f4a333f3e1a2e63c8d3e7c83fe
EC2 Default User <ec2-user@ip-172-31-78-254.ec2.internal>
2022-05-15 15:02:31 +0000

    rb_cp6-finish

---
+++ rb_cp6/lesson6-1.rb
@@ -0,0 +1,15 @@
+puts "計算をはじめます"
+puts "２つの値を入力してください"
+
+a = gets.to_i
+b = gets.to_i
+
+# input_key1 = gets
+# input_key2 = gets
+
+# a = input_key1.to_i
+# b = input_key2.to_i
+
+puts "計算結果を出力します"
+puts "a * b = #{a * b}"
+puts "計算を終了します"
